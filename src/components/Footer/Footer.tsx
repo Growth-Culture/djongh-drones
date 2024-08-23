6c514898d0582620a45aa2c19c56f08de4b1fde6
Yara Daudt <yaracrisalis@gmail.com>
2024-08-23 10:46:00 +0100

    feat: footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,40 @@
-import { Container } from  './FooterStyles'
+import { Link } from 'react-router-dom'
+import { Container, FooterLogo, UpperFooter, Copyright, FooterLinks, FooterLink, SocialMedia } from  './FooterStyles'
+import logoFooter from '../../assets/imgs/logo-djongh-footer.png'
+import { FacebookIcon } from '../../assets/icons/social-icons/fb-icon'
+import { InstagramIcon } from '../../assets/icons/social-icons/ig-icon'
+import { YoutubeIcon } from '../../assets/icons/social-icons/yt-icon'
 
 export function Footer(){
     return (
         <Container>
-            <div className='upper-footer'>
-                <div>Logo</div>
-                <div>Links</div>
-                <div>Social</div>
-            </div>
+            <UpperFooter>
+                <FooterLogo src={logoFooter} />
+                <FooterLinks>
+                    <FooterLink to="/">Início</FooterLink>
+                    <FooterLink to="/sobre-nos">Sobre nós</FooterLink>
+                    <FooterLink to="/servicos">Segmentos</FooterLink>
+                    <FooterLink to="/contact">Contatos</FooterLink>
+                </FooterLinks>
 
-            <div className='copyright'>
-                <p>2024 DeJongh Drones.</p>
-            </div>
+                <SocialMedia>
+                    <Link to="#">
+                        <InstagramIcon />
+                    </Link>
+
+                    <Link to="#">
+                        <FacebookIcon />
+                    </Link>
+
+                    <Link to="#">
+                        <YoutubeIcon />
+                    </Link>
+                </SocialMedia>
+            </UpperFooter>
+
+            <Copyright>
+                <p>&copy; 2024 DeJongh Drones.</p>
+            </Copyright>
         </Container>
     )
 }
--- src/components/Footer/FooterStyles.tsx
+++ src/components/Footer/FooterStyles.tsx
@@ -1,5 +1,57 @@
-import styled from 'styled-components'
+import { Link } from "react-router-dom"
+import styled from "styled-components"
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: fixed;
     
+    width: 100%;
+    bottom: 0;
+    height: 24rem;
+    
+    background-color: var(--primary-color);
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    font-size: 1.4rem;
+    line-height: 2.4rem;
+    color: #FFFFFF;
+    `
+
+export const UpperFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5rem 8.9rem 0 8.9rem;
+    
+`
+
+export const FooterLogo = styled.img`
+    height: 11.8rem;
+`
+
+export const FooterLinks = styled.ul`
+    display: flex;
+    justify-content: center;
+    gap: 2.8rem;
+    
+    align-items: center;
+    `
+export const FooterLink = styled(Link)`
+    text-transform: uppercase;
+    align-items: center;
+`
+
+export const SocialMedia = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 1.5rem;
+    
+    `
+export const Copyright = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
 `
