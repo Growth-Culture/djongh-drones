6544d1887e52bf909ec4dc31f42439610696d6a2
Yara Daudt <yaracrisalis@gmail.com>
2024-09-04 18:58:34 +0100

    feat: Pages Hero Component

--- src/pages/services/servicesPage.tsx
+++ src/pages/services/servicesPage.tsx
@@ -1,5 +1,14 @@
+import { HeroPages } from  '../../components/HeroPages/HeroPages'
+import servicesImage from '../../assets/imgs/hero-page.png'
+
 export function ServicesPage() {
   return (
+    <>
+    <HeroPages 
+    title='Segmentos'
+    subtitle="Elevate your content with our high-quality drone footage"
+    backgroundImage={servicesImage} />
     <h1>Services Page</h1>
+    </>
   )
 }
