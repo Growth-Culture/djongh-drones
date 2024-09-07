0e067a1ed682243929fd46fc3b2633e3c11c86c3
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2024-09-07 17:13:33 +0100

    create video and text section

---
+++ src/pages/about/components/TextAndVideoSection/TextAndVideoStyles.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100%;
+max-width: 144rem;
+margin: 0 auto;
+padding: 9rem 8.4rem;
+
+display: flex;
+justify-content: space-between;  
+align-items: center; 
+gap: 11rem;
+`
+
+export const TextWrapper = styled.div`
+width: 50%;
+display: flex;
+flex-direction: column;
+`
+
+export const SectionTag = styled.p`
+font-size: 1.8rem;
+font-weight: 500;
+color: var(--primary-color);
+`
+
+export const SectionTitle = styled.h2`
+font-size: 5rem;
+font-weight: 500;
+margin-top: 1rem;
+margin-bottom: 3rem;
+`
+export const SectionDescription = styled.p`
+font-size: 1.6rem;
+font-weight: 300;
+line-height: 2rem;
+margin-bottom: 3.6rem;
+`
+
+export const VideoWrapper = styled.div`
+width: 50%;
+
+`
+
+export const ServiceVideo = styled.video`
+width: 100%;
+height: 49rem;
+object-fit: cover;
+border-radius: 1.2rem;
+`
