e1a0929ae9dc84be6cef7807aae0e4d4d9e934e0
Yara Daudt <yaracrisalis@gmail.com>
2024-09-07 20:41:11 +0100

    feat: partners section/contact

---
+++ src/pages/contact/components/PartnershipSection/PartnershipSection.tsx
@@ -0,0 +1,11 @@
+import { PartnershipWrapper } from  './PartnershipSectionStyles';
+import Partners from '../../assets/partners.svg';
+
+export function PartnershipSection() {
+    return (
+        <>
+            <PartnershipWrapper src={Partners}/>
+
+        </>
+    )
+}
