40757418ac95785e6c00216a2f6f0216cc797a9f
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2024-09-09 20:36:19 +0100

    create menu mobile useEffect to close menu after click on link and adjusts on about page

--- src/pages/contact/components/PartnershipSection/PartnershipSectionStyles.tsx
+++ src/pages/contact/components/PartnershipSection/PartnershipSectionStyles.tsx
@@ -1,11 +1,21 @@
 import styled from "styled-components";
 
+const breakPoints = {
+    mobile: "48rem",
+    tablet: "102.4rem",
+  };
+
 export const PartnershipWrapper = styled.img`
     display: flex;
     justify-content: center;
     align-items: center;
 
     padding: 6.6rem 16.5rem;
+
+    @media (max-width: ${breakPoints.mobile}) {
+        width: 100%;
+        display: none;
+    }
     
 `
 
