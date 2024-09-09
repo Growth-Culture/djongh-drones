8382c142b2c64de4592fc249eeab7d2590c99072
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2024-09-09 19:49:59 +0100

    fix: add playsInline on all videos

--- src/pages/services/components/VideoSection/VideoSection.tsx
+++ src/pages/services/components/VideoSection/VideoSection.tsx
@@ -4,7 +4,7 @@ import SectionVideo from '../../../../assets/background_video.mp4'
 export function VideoSection() {
     return (
         <>
-        <DemonstrativeVideo autoPlay loop muted>
+        <DemonstrativeVideo autoPlay loop muted playsInline>
             <source src={SectionVideo} type="video/mp4"/>
         </DemonstrativeVideo>
         </>
