99dc2af8bff4fc59f9e278a1740f8d789d1fd98b
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-01-18 21:05:34 +0000

    fix: modal mobile: width

--- src/pages/services/components/ServiceModal/modalStyles.tsx
+++ src/pages/services/components/ServiceModal/modalStyles.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+const breakPoints = {
+  mobile: "48rem",
+};
+
 export const ModalOverlay = styled.div`
   position: fixed;
   top: 0;
@@ -21,7 +25,10 @@ export const ModalContent = styled.div`
   max-width: 500px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
   position: relative;
- 
+
+  @media (max-width: ${breakPoints.mobile}) {
+    width: 90%;
+  }
 `;
 
 export const CloseButton = styled.button`
