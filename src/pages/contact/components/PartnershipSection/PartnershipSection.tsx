40e2550db6cd09594087ba01d4e57b2eea4fcb24
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-01-25 20:22:25 +0000

    update: update info on about us page and add clients and equipaments logos

--- src/pages/contact/components/PartnershipSection/PartnershipSection.tsx
+++
@@ -1,8 +0,0 @@
-import { PartnershipWrapper } from  './PartnershipSectionStyles';
-import Partners from '../../../../assets/icons/contact-icons/partners.svg';
-
-export function PartnershipSection() {
-    return (
-        <PartnershipWrapper src={Partners}/>
-    )
-}
